c95917e0dedb8f6a03fcdc45fac5045bf7a540e4
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-07 15:02:50 -0300

    💄 update theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,26 @@
 import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
-  palette: { primary: { main: '#219653' } },
-  typography: { fontFamily: 'Montserrat' },
+  palette: {
+    type: 'dark',
+    primary: { main: '#39E991', contrastText: 'white' },
+    secondary: { main: '#219653' },
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    button: {
+      fontWeight: 600,
+    },
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+  },
 });
 
 export default theme;
