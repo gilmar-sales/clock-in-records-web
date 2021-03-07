0f0f4d0fc359fb3952e741ea9b052bdcd60df9de
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-07 11:21:46 -0300

    ✨ add montserrat font

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: { primary: { main: '#219653' } },
+  typography: { fontFamily: 'Montserrat' },
+});
+
+export default theme;
