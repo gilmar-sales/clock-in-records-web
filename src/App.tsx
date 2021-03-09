728f7b0a1f715413b090c4d00aab84c103fcfbba
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-08 21:39:05 -0300

    ✨ add apollo client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
+import { ApolloProvider } from '@apollo/client';
 
+import api from './services/api';
 import Routes from './Routes';
 
 function App() {
   return (
     <Box>
-      <Routes />
+      <ApolloProvider client={api}>
+        <Routes />
+      </ApolloProvider>
     </Box>
   );
 }
