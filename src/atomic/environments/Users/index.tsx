f59d902c82e6a27e6be03ea0e6d9734af4575c9f
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-09 14:17:22 -0300

    ✨ fetch users from graphql api

--- src/atomic/environments/Users/index.tsx
+++ src/atomic/environments/Users/index.tsx
@@ -1,17 +1,38 @@
+import { gql, useQuery } from '@apollo/client';
 import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { User } from '../../../@types/user';
 import UserList from '../../organisms/UserList';
 
 import useStyles from './styles';
 
+const LIST_USERS = gql`
+  query listUsers {
+    listUsers {
+      id
+      name
+      email
+      role
+    }
+  }
+`;
+
 const Users: React.FC = () => {
   const classes = useStyles();
+  const [users, setUsers] = useState<User[]>([]);
+  // const users: User[] = [
+  //   { id: 1, name: 'Gilmar', email: '', role: 'admin' },
+  //   { id: 2, name: 'Paulo', email: '', role: 'admin' },
+  // ];
 
-  const users: User[] = [
-    { id: 1, name: 'Gilmar', email: '', role: 'admin' },
-    { id: 2, name: 'Paulo', email: '', role: 'admin' },
-  ];
+  const { data } = useQuery(LIST_USERS);
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data.listUsers);
+      console.log(data.listUsers);
+    }
+  }, [data]);
 
   return (
     <div className={classes.root}>
--- src/atomic/organisms/UserList/index.tsx
+++ src/atomic/organisms/UserList/index.tsx
@@ -15,8 +15,9 @@ interface UserListProps {
   data: User[];
 }
 
-const UserList: React.FC<UserListProps> = () => {
+const UserList: React.FC<UserListProps> = ({ data }) => {
   const classes = useStyles();
+
   return (
     <TableContainer className={classes.container}>
       <Table size="small" className={classes.table}>
@@ -30,23 +31,27 @@ const UserList: React.FC<UserListProps> = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow className={classes.bodyRow}>
-            <TableCell className={classes.firstCell}>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <div className={classes.decoration} />
-                <div className={classes.name}>
-                  Gilmar
-                  <div className={classes.number}>001</div>
+          {data.map((user) => (
+            <TableRow key={user.id} className={classes.bodyRow}>
+              <TableCell className={classes.firstCell}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <div className={classes.decoration} />
+                  <div className={classes.name}>
+                    {user.name}
+                    <div className={classes.number}>
+                      {Number(user.id).toString().padStart(3, '0')}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </TableCell>
-            <TableCell className={classes.bodyCell}>
-              <span className={classes.date}>gilmar_custodio_@hotmail.com</span>
-            </TableCell>
-            <TableCell className={classes.lastCell}>
-              <span className={classes.hour}>Collaborator</span>
-            </TableCell>
-          </TableRow>
+              </TableCell>
+              <TableCell className={classes.bodyCell}>
+                <span className={classes.date}>{user.email}</span>
+              </TableCell>
+              <TableCell className={classes.lastCell}>
+                <span className={classes.hour}>{user.role}</span>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
