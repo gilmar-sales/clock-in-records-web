7c312b6c0556a7956229f62936358c84fde45128
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-09 15:32:10 -0300

    ✨ add web socket link to graphql

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,36 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://192.168.1.4:3333/graphql', // use wss for a secure endpoint
+  options: {
+    reconnect: true,
+  },
+});
 
 const httpLink = createHttpLink({
   uri: 'http://192.168.1.4:3333/graphql',
 });
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const authLink = setContext((_, { headers }) => {
   const { token } = JSON.parse(localStorage.getItem('@token_payload') || '{}');
 
@@ -17,7 +44,7 @@ const authLink = setContext((_, { headers }) => {
 
 const api = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
 });
 
 export default api;
