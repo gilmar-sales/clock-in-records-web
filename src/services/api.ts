ecb204555d6b82b07f5786e0e9d46829de585ef8
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-09 12:06:57 -0300

    ✨ add graphql api connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://192.168.1.4:3333/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const { token } = JSON.parse(localStorage.getItem('@token_payload') || '{}');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const api = new ApolloClient({
-  uri: 'http://192.168.1.4:3333/graphql',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default api;
