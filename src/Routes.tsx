765312ab5fb5d7471d6b4863807851004ac18c4b
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-08 21:43:03 -0300

    ✨ add routes to environments

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,21 +3,39 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import TabsPanel from './atomic/ecosystems/TabsPanel';
 
 import LoginEnvironment from './atomic/environments/Login';
+import RegistersEnvironment from './atomic/environments/Registers';
+import DashboardEnvironment from './atomic/environments/Dashboard';
+import UsersEnvironment from './atomic/environments/Users';
+
+import { AuthContextProvider } from './contexts/auth';
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={LoginEnvironment} />
-        <Route
-          path="/user"
-          component={({ match }: { match: any }) => (
-            <TabsPanel>
-              <Route path={`${match.path}/`} render={() => 'registers'} />
-            </TabsPanel>
-          )}
-        />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/login" component={LoginEnvironment} />
+          <Route
+            path="/panel"
+            component={({ match }: { match: any }) => (
+              <TabsPanel>
+                <Route
+                  path={`${match.path}/registers`}
+                  component={RegistersEnvironment}
+                />
+                <Route
+                  path={`${match.path}/dashboard`}
+                  component={DashboardEnvironment}
+                />
+                <Route
+                  path={`${match.path}/users`}
+                  component={UsersEnvironment}
+                />
+              </TabsPanel>
+            )}
+          />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
