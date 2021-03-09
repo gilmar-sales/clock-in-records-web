e0f07a706d24a8979dc9d62480ae6120949bb6ed
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-09 17:10:24 -0300

    ✨ add drawer to create register

--- src/atomic/environments/Registers/index.tsx
+++ src/atomic/environments/Registers/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from '@material-ui/core';
-import { gql, useQuery } from '@apollo/client';
+import { Backdrop, Button, Drawer, TextField } from '@material-ui/core';
+import { gql, useMutation, useQuery } from '@apollo/client';
 
 import { Register } from '../../../@types/register';
 import RegisterList from '../../organisms/RegisterList';
@@ -20,10 +20,29 @@ const LIST_USER_REGISTERS = gql`
   }
 `;
 
+const CREATE_REGISTER = gql`
+  mutation createRegister($date: DateTime!) {
+    createRegister(timeRegistered: $date) {
+      id
+      timeRegistered
+      user {
+        id
+        name
+        email
+        role
+      }
+    }
+  }
+`;
+
 const Registers: React.FC = () => {
   const [registers, setRegisters] = useState<Register[]>([]);
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
   const classes = useStyles();
   const { data } = useQuery(LIST_USER_REGISTERS);
+  const [createRegister] = useMutation(CREATE_REGISTER);
+
+  const [date, setDate] = useState('');
 
   useEffect(() => {
     if (data) {
@@ -31,13 +50,74 @@ const Registers: React.FC = () => {
     }
   }, [data]);
 
+  const handleSubmit = () => {
+    setDrawerOpen(false);
+
+    createRegister({ variables: { date } })
+      .then((response) => {
+        setRegisters([response.data.createRegister, ...registers]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setDate('');
+  };
+
   return (
     <div className={classes.root}>
+      <Backdrop className={classes.backdrop} open={isDrawerOpen}>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={isDrawerOpen}
+          elevation={2}
+          classes={{ paper: classes.drawer }}
+        >
+          <div>
+            <h2 className={classes.title}>New Register</h2>
+            <div className={classes.form}>
+              <div>Collaborator</div>
+              <h1 className={classes.userName}>Gilmar</h1>
+
+              <div>Date & Hour</div>
+              <TextField
+                className={classes.date}
+                type="datetime-local"
+                color="secondary"
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className={classes.bottom}>
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              disabled={date.length === 0}
+              onClick={() => handleSubmit()}
+            >
+              Register
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onClick={() => setDrawerOpen(false)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Drawer>
+      </Backdrop>
       <div className={classes.menu}>
         <Button
           variant="contained"
           color="secondary"
           className={classes.button}
+          onClick={() => setDrawerOpen(!isDrawerOpen)}
         >
           Register
         </Button>
--- src/atomic/environments/Registers/styles.ts
+++ src/atomic/environments/Registers/styles.ts
@@ -17,6 +17,39 @@ const useStyles = makeStyles((theme) => ({
   button: {
     fontSize: '1.5rem',
   },
+  backdrop: {
+    zIndex: 100,
+  },
+  drawer: {
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 400,
+    },
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontWeight: 100,
+    padding: theme.spacing(0, 3, 2),
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  form: {
+    padding: theme.spacing(3, 6),
+  },
+  userName: {
+    fontWeight: 400,
+  },
+  date: {
+    width: 200,
+    padding: theme.spacing(2, 0),
+  },
+  bottom: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: '1rem',
+    padding: '1rem',
+  },
 }));
 
 export default useStyles;
