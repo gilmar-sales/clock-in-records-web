07bf24814839d0d0dfad4ba6fcb60b141a0ae819
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-11 18:13:59 -0300

    ✨ validate login form with react hook form

--- src/atomic/environments/Login/index.tsx
+++ src/atomic/environments/Login/index.tsx
@@ -1,16 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import {
   Box,
   Button,
   Grid,
   Paper,
-  Snackbar,
   TextField,
   Typography,
   useTheme,
 } from '@material-ui/core';
-import MuiAlert, { Color } from '@material-ui/lab/Alert';
 import { gql, useMutation } from '@apollo/client';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import LogoIcon from '../../atoms/LogoIcon';
 
@@ -32,14 +33,15 @@ const LOGIN = gql`
   }
 `;
 
-const Login: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(6).required(),
+});
 
-  const [isFeedbackOpen, setFeebackOpen] = useState(false);
-  const [requestMessage, setRequestMessage] = useState('');
-  const [requestSeverity, setRequestSeverity] = useState<Color>();
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+const Login: React.FC = () => {
+  const { control, errors, handleSubmit, setError } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const classes = useStyles();
   const theme = useTheme();
@@ -47,27 +49,14 @@ const Login: React.FC = () => {
 
   const [login] = useMutation(LOGIN);
 
-  const showMessage = (severity: Color, message: string) => {
-    if (intervalId !== undefined) clearInterval(intervalId);
-
-    setRequestSeverity(severity);
-    setRequestMessage(message);
-    setFeebackOpen(true);
-    setIntervalId(setInterval(() => setFeebackOpen(false), 3000));
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    login({ variables: { email, password } })
+  const onSubmit = (data: any) => {
+    login({ variables: data })
       .then((response) => {
-        console.log(response.data.login as TokenPayload);
         authCtx.handleLogin(response.data.login as TokenPayload);
-
-        showMessage('success', 'Successfully logged in');
       })
       .catch((error) => {
-        showMessage('error', error.message);
+        const [field, message] = error.message.split(':');
+        setError(field, { message });
       });
   };
 
@@ -79,12 +68,6 @@ const Login: React.FC = () => {
       alignItems="center"
       justify="space-between"
     >
-      <Snackbar
-        anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
-        open={isFeedbackOpen}
-      >
-        <MuiAlert severity={requestSeverity}>{requestMessage}</MuiAlert>
-      </Snackbar>
       <Grid item xs={12} md={6} className={classes.logo}>
         <LogoIcon width={200} height={200} color={theme.palette.primary.main} />
         <Typography variant="h3">Register</Typography>
@@ -99,32 +82,34 @@ const Login: React.FC = () => {
       </Grid>
       <Grid item xs={12} md={5} className={classes.content}>
         <Paper component={Paper} elevation={0} square className={classes.paper}>
-          <form onSubmit={handleSubmit}>
-            <TextField
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              as={TextField}
               variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
               name="email"
-              color="secondary"
-              autoComplete="email"
-              autoFocus
-              onChange={(event) => setEmail(event.target.value)}
+              label="Email"
+              defaultValue=""
+              fullWidth
+              required
+              color={!errors.email ? 'secondary' : undefined}
+              control={control}
+              error={Boolean(errors.email)}
+              helperText={errors.email?.message}
             />
-            <TextField
+            <Controller
+              as={TextField}
               variant="outlined"
               margin="normal"
-              required
-              fullWidth
-              id="password"
               label="Password"
               type="password"
               name="password"
-              color="secondary"
-              autoComplete="password"
-              onChange={(event) => setPassword(event.target.value)}
+              defaultValue=""
+              fullWidth
+              required
+              color={!errors.password ? 'secondary' : undefined}
+              control={control}
+              error={Boolean(errors.password)}
+              helperText={errors.password?.message}
             />
             <Button
               type="submit"
