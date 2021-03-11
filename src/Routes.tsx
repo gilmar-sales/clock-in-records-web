32246e4fc2e1cf24b1b6745b082e16607183c986
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-11 18:13:20 -0300

    ✨ add protected route to routes that auth is required

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,29 +1,61 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Switch, Route, RouteProps, Redirect } from 'react-router-dom';
 import TabsPanel from './atomic/ecosystems/TabsPanel';
 
 import LoginEnvironment from './atomic/environments/Login';
 import RegistersEnvironment from './atomic/environments/Registers';
 import DashboardEnvironment from './atomic/environments/Dashboard';
 import UsersEnvironment from './atomic/environments/Users';
+import AuthContext from './contexts/auth';
+
+interface ProtectedRouteProps extends RouteProps {
+  admin?: boolean;
+}
 
 export default function Routes() {
+  const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+    component,
+    admin,
+    ...rest
+  }) => {
+    const authCtx = useContext(AuthContext);
+
+    if (!authCtx.isAuthenticated())
+      return (
+        <Redirect to={{ pathname: '/', state: { from: rest.location } }} />
+      );
+
+    const routeComponent = (props: any) =>
+      admin && !authCtx.isAdmin() ? (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      ) : (
+        React.createElement(component as React.FunctionComponent, props)
+      );
+
+    return <Route {...rest} render={routeComponent} />;
+  };
+
   return (
     <Switch>
       <Route path="/" exact component={LoginEnvironment} />
-      <Route
+      <ProtectedRoute
         path="/panel"
         component={({ match }: { match: any }) => (
           <TabsPanel>
-            <Route
+            <ProtectedRoute
               path={`${match.path}/registers`}
               component={RegistersEnvironment}
             />
-            <Route
+            <ProtectedRoute
+              admin
               path={`${match.path}/dashboard`}
               component={DashboardEnvironment}
             />
-            <Route path={`${match.path}/users`} component={UsersEnvironment} />
+            <ProtectedRoute
+              admin
+              path={`${match.path}/users`}
+              component={UsersEnvironment}
+            />
           </TabsPanel>
         )}
       />
