b183d053acbdf63cd01fc7c6adf722d0718c979b
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-12 07:18:16 -0300

    💄 colorize date picker

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,59 @@
 import { createMuiTheme } from '@material-ui/core';
 
+import { MuiPickersOverrides } from '@material-ui/pickers/typings/overrides';
+
+type overridesNameToClassKey = {
+  [P in keyof MuiPickersOverrides]: keyof MuiPickersOverrides[P];
+};
+
+declare module '@material-ui/core/styles/overrides' {
+  export interface ComponentNameToClassKey extends overridesNameToClassKey {}
+}
+
 const theme = createMuiTheme({
   palette: {
-    primary: { main: '#39E991', contrastText: 'white' },
-    secondary: { main: '#219653', contrastText: 'white' },
+    primary: { main: '#39E991', contrastText: '#fff' },
+    secondary: { main: '#219653', contrastText: '#fff' },
+  },
+  overrides: {
+    MuiPickersToolbar: {
+      toolbar: {
+        backgroundColor: '#219653',
+      },
+    },
+    MuiPickerDTTabs: {
+      tabs: {
+        backgroundColor: '#219653',
+      },
+    },
+    MuiPickersDay: {
+      daySelected: {
+        backgroundColor: '#219653',
+      },
+      current: {
+        color: '#219653',
+      },
+    },
+    MuiPickersClock: {
+      pin: {
+        backgroundColor: '#219653',
+      },
+    },
+    MuiPickersClockNumber: {
+      clockNumberSelected: {
+        backgroundColor: '#219653',
+      },
+    },
+    MuiPickersClockPointer: {
+      pointer: {
+        backgroundColor: '#219653',
+      },
+    },
+    MuiPickersModal: {
+      dialogAction: {
+        color: '#219653',
+      },
+    },
   },
   typography: {
     fontFamily: 'Montserrat',
